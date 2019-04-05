8ad78d0941603f8fb5e8376d63b6a24fb920e493
1alena123 <44710795+1alena123@users.noreply.github.com>
2019-04-05 15:17:38 +0700

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AllMethod m = new AllMethod();
+        m.method();
+        m.method2();
+        m.method3();
+    }
+}
---
+++ src/com/company/NonGen.java
@@ -0,0 +1,18 @@
+package com.company;
+
+class NonGen {
+    Object ob;
+
+    NonGen(Object o) {
+        ob = o;
+    }
+
+    Object getob() {
+        return ob;
+    }
+
+    void showТype() {
+        System.out.println("Oбъeкт оЬ относится к типу " +
+                ob.getClass().getName());
+    }
+}
